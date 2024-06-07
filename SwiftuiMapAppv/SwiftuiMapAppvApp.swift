cd618bae3fa036d35c490d38696433f16c53d703
Baisampayan <baisampayans@gmail.com>
2024-06-07 23:29:46 +0530

    Initial Commit

---
+++ SwiftuiMapAppv/SwiftuiMapAppvApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiMapAppvApp.swift
+//  SwiftuiMapAppv
+//
+//  Created by baisampayan saha on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiMapAppvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
